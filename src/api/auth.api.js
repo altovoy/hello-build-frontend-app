9b6746cf006a1c2e196661145ff8b73bf9f6cadf
Alejandro Torres <altovoy@gmail.com>
2022-08-30 12:23:11 -0500

    FEATURE/SIGN_UP signup lamba service integration

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,10 +1,20 @@
+import axios from "axios";
+
+const baseUri = process.env.REACT_APP_MAIN_API_URI;
+
+const authUri = `${baseUri}/auth`;
+
 export const login = async ({ username, password }) => {
   return true;
 };
 
-export const signup = async ({ username, password }) => {
-  return true;
-};
+export const signup = ({ userName, password }) =>
+  axios.post(`${authUri}/signup`, {
+    user: {
+      userName,
+      password,
+    },
+  });
 
 export const logout = async () => {
   return true;
